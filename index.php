42d61bacfe183f55135ea326a040e1f710088da2
unknown <181102505@LAB404-04.labs.senacrs.com.br>
2015-10-14 21:25:38 -0300

    Testes realizados com nova API window.matchMedia.

--- index.php
+++ index.php
@@ -3,6 +3,32 @@
 <head>
 	<title>Teste</title>
 	<meta charset="utf-8" />
+	<script type="application/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript">
+	
+		$(document).ready(ready);
+		
+		function ready() {
+			if (window.matchMedia) {
+		
+				var mediaQuery = window.matchMedia('(max-width: 700px)');
+				mediaQuery.addListener(resize);
+				resize(mediaQuery);
+			}		
+		}
+		
+		function resize(mediaQuery) {
+		
+			if (mediaQuery.matches) {
+			
+				console.log('Mobile');
+			}
+			else {
+			
+				console.log('Desktop');
+			}
+		}
+	</script>
 </head>
 <body>
 	<h1>Teste</h1>
