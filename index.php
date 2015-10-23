244ac2ee02998bdd8edb74dd6121b7e246bd243a
Lucas Inácio Viegas <lucasdsaiv.4191@hotmail.com>
2015-10-23 19:56:58 -0200

    Cabeçalho padrão movido para um arquivo próprio.

--- index.php
+++ index.php
@@ -1,36 +1,42 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Teste</title>
-	<meta charset="utf-8" />
-	<script type="application/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script type="text/javascript">
-	
-		$(document).ready(ready);
-		
-		function ready() {
-			if (window.matchMedia) {
-		
-				var mediaQuery = window.matchMedia('(max-width: 700px)');
-				mediaQuery.addListener(resize);
-				resize(mediaQuery);
-			}		
-		}
-		
-		function resize(mediaQuery) {
-		
-			if (mediaQuery.matches) {
-			
-				console.log('Mobile');
-			}
-			else {
-			
-				console.log('Desktop');
-			}
-		}
-	</script>
-</head>
+<?php
+
+    include 'public/header.php';
+    
+    echoHeader('Teste');
+?>
+
 <body>
-	<h1>Teste</h1>
+    <script type="text/javascript">
+	
+        $(document).ready(ready);
+		
+        function ready() {
+            if (window.matchMedia) {
+		
+                var mediaQuery = window.matchMedia('(max-width: 700px)');
+                mediaQuery.addListener(resize);
+                resize(mediaQuery);
+            }		
+        }
+		
+        function resize(mediaQuery) {
+		
+            if (mediaQuery.matches) {
+			
+                console.log('Mobile');
+            }
+            else {
+			
+                console.log('Desktop');
+            }
+        }
+		
+        function setPage() {
+		
+            
+        }
+    </script>
 </body>
 </html>
---
+++ public/header.php
@@ -0,0 +1,11 @@
+<?php
+
+function echoHeader($title) {
+
+    echo '<head>' .
+         '<meta charset="utf-8" />' .
+         '<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>' .
+         '</head>';
+}
+
+?>
