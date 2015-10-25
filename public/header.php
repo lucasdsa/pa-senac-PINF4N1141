22e88309013abfb58522c5424907632d1b9f49ff
Lucas Inácio Viegas <lucasdsaiv.4191@hotmail.com>
2015-10-25 17:49:57 -0200

    Adicionada tag meta viewport no cabecalho

--- public/header.php
+++ public/header.php
@@ -4,6 +4,7 @@ function echoHeader($title) {
 
     echo '<head>' .
          '<meta charset="utf-8" />' .
+         '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />' .
          '<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>' .
          '<link rel="stylesheet" href="public/css/index.css" />' .
          '<link rel="stylesheet" href="public/css/decoration.css" />' .
