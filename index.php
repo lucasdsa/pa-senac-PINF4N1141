f44e33ffa1db569197a1e1db18fbc0084734b95c
Lucas Inácio Viegas <lucasdsaiv.4191@hotmail.com>
2015-10-28 19:32:46 -0200

    Experimentando com o visual do site

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 <ul id="menulist">
                     <li class="menuitem"><a id="home" href="/">Página inicial</a></li>
                     <li class="menuitem"><a id="articles" href="#">Artigos</a></li>
-                    <li class="menuitem"><a id="game" href="tictactoe.html">Competências</a></li>
+                    <li class="menuitem"><a id="game" href="#">Competências</a></li>
                     <li class="menuitem"><a id="articles" href="#">Experiências</a></li>
                     <li class="menuitem"><a id="articles" href="#">Interesses</a></li>
                     <li class="menuitem"><a id="articles" href="#">Pessoal</a></li>
