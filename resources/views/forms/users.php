a8f515bede0b0ed9d41b9208f5a364b292551768
Lucas Inácio Viegas <lucasdsaiv.4191@hotmail.com>
2015-11-16 21:42:14 -0200

    Changes on user view layout

--- resources/views/forms/users.php
+++ resources/views/forms/users.php
@@ -1,10 +1,10 @@
 <?php
 
 foreach ($users as $user) {
-    echo '<div>' . $user->name . '</div>';
+    echo '<div class="user">' . $user->name . '</div>';
 }
 
-echo '<a id="listPrev" href="#">&lt</a>' .
+echo '<br /><a id="listPrev" href="#">&lt</a>' .
      '<a id="listNext" href="#">&gt</a>';
 
 ?>
